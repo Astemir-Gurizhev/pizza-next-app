e51de619c8f00d1bcc55c5b9e770c31d6b2935cf
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-03-18 20:04:34 +0300

    Add Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
-	return (
-		<>
-			<h1>Home</h1>
-			<Button variant='outline'>Click me</Button>
-		</>
-	)
+	return <><h1>Главная</h1></>
 }
