5c5c50f50c5a61a4e81f0ea85d09d568675080f6
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-03-21 15:23:52 +0300

    Add Products Group List

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Container, Filters, Title, TopBar } from '@/components/shared'
-import { ProductCard } from '@/components/shared/product-card'
+import { ProductsGroupList } from '@/components/shared/products-group-list'
 
 export default function Home() {
 	return (
@@ -18,9 +18,62 @@ export default function Home() {
 
 					<div className='flex-1'>
 						<div className='flex flex-col gap-16'>
-							{/* <ProductsGroupList title='Пиццы' items={[1,2,3,4,5]} />
-							<ProductsGroupList title='Пиццы' items={[1,2,3,4,5]} /> */}
-							
+							<ProductsGroupList
+								title='Пиццы'
+								categoryId={1}
+								items={[
+									{
+										id: 1,
+										name: 'Чизбургер-пицца',
+										imageUrl: 'https://media.dodostatic.net/image/r:292x292/11ee7d61698827ee9b8db6d0aec53410.avif',
+										price: 550,
+										items: [{price:550}],
+									},
+									{
+										id: 1,
+										name: 'Чизбургер-пицца',
+										imageUrl: 'https://media.dodostatic.net/image/r:292x292/11ee7d61698827ee9b8db6d0aec53410.avif',
+										price: 550,
+										items: [{price:550}],
+									},
+									{
+										id: 1,
+										name: 'Чизбургер-пицца',
+										imageUrl: 'https://media.dodostatic.net/image/r:292x292/11ee7d61698827ee9b8db6d0aec53410.avif',
+										price: 550,
+										items: [{price:550}],
+									},
+									{
+										id: 1,
+										name: 'Чизбургер-пицца',
+										imageUrl: 'https://media.dodostatic.net/image/r:292x292/11ee7d61698827ee9b8db6d0aec53410.avif',
+										price: 550,
+										items: [{price:550}],
+									},
+									{
+										id: 1,
+										name: 'Чизбургер-пицца',
+										imageUrl: 'https://media.dodostatic.net/image/r:292x292/11ee7d61698827ee9b8db6d0aec53410.avif',
+										price: 550,
+										items: [{price:550}],
+									},
+									{
+										id: 1,
+										name: 'Чизбургер-пицца',
+										imageUrl: 'https://media.dodostatic.net/image/r:292x292/11ee7d61698827ee9b8db6d0aec53410.avif',
+										price: 550,
+										items: [{price:550}],
+									},
+									{
+										id: 1,
+										name: 'Чизбургер-пицца',
+										imageUrl: 'https://media.dodostatic.net/image/r:292x292/11ee7d61698827ee9b8db6d0aec53410.avif',
+										price: 550,
+										items: [{price:550}],
+									},
+									
+								]}
+							/>
 						</div>
 					</div>
 				</div>
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from 'react'
+import { Title } from './title'
+import { cn } from '@/lib/utils'
+import { ProductCard } from './product-card'
 
 interface Props {
-	className?: string;
+	title: string
+	items: any[]
+	listClassName?: string
+	categoryId: number
+	className?: string
 }
 
-export const ProductsGroupList: React.FC<Props> = ({ className }) => {
-	return (
-		<div className={className}></div>
-	);
-};
+export const ProductsGroupList: React.FC<Props> = ({
+	title,
+	items,
+	listClassName,
+	categoryId,
+	className,
+}) => {
+	return <div className={className}>
+		<Title text={title} size='lg' className='font-extrabold mb-5'/>
+
+		<div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+			{items.map((product, index) => (
+				<ProductCard 
+					key={product.id}
+					id={product.id}
+					name={product.name}
+					imageUrl={product.imageUrl}
+					price={product.items[0].price}
+				/>
+			))}
+
+		</div>
+	</div>
+}
